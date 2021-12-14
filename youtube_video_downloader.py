ee4f39c703eec83b9bb00511cd4dd74bdc23adc8
Kazi Sohan <kazisohan1920@gmail.com>
2021-12-14 23:25:01 +0600

    Create youtube_video_downloader.py
    Asks the user to input a youtube video link, download the youtube video if the link is valid, and save it as an mp4 file.

---
+++ youtube_video_downloader.py
@@ -0,0 +1,23 @@
+from pytube import YouTube
+from pytube.cli import on_progress
+
+
+link= input("Enter youtube video link: ")#"https://www.youtube.com/watch?v=_j1CWOhnotg"
+SAVE_PATH = "./"
+try: 
+    yt = YouTube(link, on_progress_callback=on_progress) 
+except: 
+    print("Connection Error or invalid link") 
+    exit(0)
+
+title = yt.title + ".mp4"
+title = title.replace("/", "")
+title = title.replace("\\", "")
+title = title.replace("|", "")
+print(title)
+try:
+    yt.streams.filter(file_extension = "mp4", progressive = True).first().download(filename = title, output_path = SAVE_PATH) 
+    print('\n\nTask Completed!') 
+except: 
+    print("Some Error Occured!") 
+    
