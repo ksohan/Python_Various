57921830e9815a2748b520dd52f2dd43a17f32c5
Kazi Sohan <kazisohan1920@gmail.com>
2021-12-14 23:28:11 +0600

    Create Audio_slice.py
    Reads an mp4 audio files and slices it into 10 second chuncks

---
+++ Audio_slice.py
@@ -0,0 +1,17 @@
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+
+audio_file_name = "Make some noise! DJI FPV Drone vs Mavic Air 2, 2 ProZoom & Mini - Noise & speed comparison"
+
+newAudio = AudioSegment.from_file(audio_file_name + ".mp4", "mp4")
+
+chunk_length_ms = 10 * 1000 # pydub calculates in millisec (10 sec)
+
+chunks = make_chunks(newAudio, chunk_length_ms)
+
+for i, chunk in enumerate(chunks):
+    chunk_name = "{0}{1}.wav".format(audio_file_name, i)
+    print("exporting", chunk_name)
+    chunk.export(chunk_name, format="wav")
+
+
